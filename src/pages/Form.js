d65e7ed89f28592e1443e1c9782cf97bcc53b2d7
Rahul vijayvergiya <vijayrahul25@gmail.com>
2018-02-14 15:56:47 +0530

    second commit

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React from "react";
 
 class Form extends React.Component {
-  state = {userName : 'First'}
+  state = { userName: "First" };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
     //console.log('form submited' + this.state.userName);
     fetch(`https://api.github.com/users/${this.state.userName}`)
       .then(resp => {
-        return resp.json();
+        if (resp.status === 200) {
+          return resp.json();
+        }
       })
-      .then((responseData) => {
-        this.props.AddNewAppCard(responseData);
+      .then(responseData => {
+        if (responseData) {
+          this.props.AddNewAppCard(responseData);
+        }
+      })
+      .catch(function() {
+        console.log("no data found");
       });
-  }
+  };
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-        <input type="text" 
-          value ={this.state.userName}
-          onChange={(event) => this.setState({ userName: event.target.value})}
-          placeholder="Github User"/>
-        <button type="submit">Submit</button>
+          <input
+            type="text"
+            value={this.state.userName}
+            onChange={event => this.setState({ userName: event.target.value })}
+            placeholder="Github User"
+          />
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
